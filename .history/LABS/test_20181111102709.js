59689344b1f1c5369ac6872f0fa16d2273bb6c7a
Semen Aliabyshev <senya18071995@gmail.com>
2018-11-14 19:05:53 +0200

    class and home work

---
+++ .history/LABS/test_20181111102709.js
@@ -0,0 +1,22 @@
+var makeCount = (function (){
+     var count = 0;
+
+     return {
+        show: (function(count) {
+            console.log(count)
+        })(count),
+        set: function() {
+            count++
+        }
+    }
+ })();
+
+ var s1 = makeCount;
+ s1.set();
+ s1.set();
+ s1.set();
+ s1.show();
+ 
+ var s2 = makeCount;
+ s2.set();
+ s2.show();
